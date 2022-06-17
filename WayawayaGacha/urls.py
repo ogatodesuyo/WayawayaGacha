94efff59049dbd317dbd8676fb5f4db7c274de2c
ogatodesuyo <tenten_flog@icloud.com>
2022-06-17 17:04:28 +0900

    トップメニューへの遷移を追加

--- WayawayaGacha/urls.py
+++ WayawayaGacha/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from menu import views
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.top_page),
     path('gacha/', include('gacha.urls')),
     path('menu/', include('menu.urls')),
 ]
--- menu/views.py
+++ menu/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse
 
 
+def top_page(request):
+    return render(request,'menu/top.html',)
+
+
 def menu_list(request):
     return render(request, 'menu/menu_list.html')
 
