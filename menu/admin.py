b3b75ef2bc6dac9766b48fb241091d63c7499d3c
ogatodesuyo <tenten_flog@icloud.com>
2022-06-18 20:10:53 +0900

    管理画面の表示設定

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from .models import Category, SubCategory, Menu
 
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_field = {'slug': ('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_field = {'slug': ('name',)}
+
+
+admin.site.register(SubCategory, SubCategoryAdmin)
+
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'available']
+    list_editable = ['price', 'available']
+    prepopulated_field = {'slug': ('name',)}
+    list_per_page = 20
+
+
+admin.site.register(Menu, MenuAdmin)
